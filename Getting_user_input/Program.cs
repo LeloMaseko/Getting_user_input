fcde72129b24344aa34327c851c9bd9a502beafc
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 13:48:34 +0200

    Add project files.

---
+++ Getting_user_input/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_user_input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            Console.WriteLine("How old are you? ");
+            string age = Console.ReadLine();
+            Console.WriteLine("You are " + age + "old");
+        }
+    }
+}
